2e39a39676ff25f58709bdc2529b4f532d934305
Meiirimm <mukhtarmeirim30@gmail.com>
2023-08-18 00:26:27 +0600

    production mode configured

--- config/config.js
+++ config/config.js
@@ -7,7 +7,12 @@ module.exports = {
       dialect: 'postgres'
     },
     production: {
-      // Конфигурация для продакшн окружения
+      username: 'doadmin',
+      password: 'AVNS_WqNzDFsGW_6FJIb3NBf',
+      database: 'defaultdb',
+      host: 'db-postgresql-sgp1-19609-do-user-14535698-0.b.db.ondigitalocean.com',
+      dialect: 'postgres',
+      port: 25060
     }
 };
   
