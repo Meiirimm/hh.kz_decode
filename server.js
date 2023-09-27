66368a153480d1fde9cb5a978befcc9bdba6e669
Meiirimm <mukhtarmeirim30@gmail.com>
2023-09-27 23:41:48 +0600

    cors

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const cors = require("cors")
 const app = express();
 
 app.use(logger('dev'))
-app.use(cors())
+const corsOptions = {
+    origin: ['http://localhost:3000', 'http://example.com'],
+  };
+  
+app.use(cors(corsOptions));
 app.use(express.urlencoded())
 app.use(express.json())
 app.use(passport.initialize());
